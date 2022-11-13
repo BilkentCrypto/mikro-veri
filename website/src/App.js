72cbe27c94865de3c3bda592f86853bdf2560eee
utkukoca <utkukoca43@gmail.com>
2022-11-13 04:34:49 +0300

    add Government

--- website/src/App.js
+++ website/src/App.js
@@ -3,15 +3,39 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Pool from "./components/Pool";
 import Footer from "./components/Footer";
+import Government from "./components/Government";
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import { Input } from "@material-tailwind/react";
+
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Header/>
-      <Pool/>
-      <Footer/>
+      
+
+      
+      <Router>
+        <Routes>
+          <Route exact path="/devlet" element={<Government/>} />
+          <Route exact path="" element={<><Navbar/><Header/><Pool/><Footer/></>}/>
+        </Routes>
+        <ul>
+            <li><Link to="/devlet">Home</Link></li>
+        
+        </ul>
+        
+        
+         
+        
+        
+      </Router>
+
+
+
+
+
     </div>
+   
   );
 }
 
