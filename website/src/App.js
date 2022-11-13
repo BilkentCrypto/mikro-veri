c8d84151bbe26533a8e110a6ef51327dfdba61c1
utkukoca <utkukoca43@gmail.com>
2022-11-13 05:10:02 +0300

    add NavbarGovernment

--- website/src/App.js
+++ website/src/App.js
@@ -4,8 +4,10 @@ import Header from "./components/Header";
 import Pool from "./components/Pool";
 import Footer from "./components/Footer";
 import Government from "./components/Government";
+import NavbarGovernment from "./components/NavbarGovernment";
+
 import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
-import { Input } from "@material-tailwind/react";
+
 
 
 function App() {
@@ -16,7 +18,7 @@ function App() {
       
       <Router>
         <Routes>
-          <Route exact path="/devlet" element={<Government/>} />
+          <Route exact path="/devlet" element={<><NavbarGovernment/><Government/></>} />
           <Route exact path="" element={<><Navbar/><Header/><Pool/><Footer/></>}/>
         </Routes>
         <ul>
--- website/src/components/ListElement.jsx
+++ website/src/components/ListElement.jsx
@@ -9,14 +9,19 @@ export default function ListElement(props) {
 
         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
 
-            <td  class="py-4 px-6 text-xl bg-transparent font-medium text-gray-900 whitespace-nowrap dark:text-white">
-               {veriTuru}
+            <th scope="row" class="py-4 px-6 text-xl font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {veriTuru}
+            </th>
+            <td class="py-4 text-xl px-6">
+                {veriIcerigi}
             </td>
+            <td class="py-4 px-6 text-right">
+             
+                        <input id="checkbox-all" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                        <label for="checkbox-all" class="sr-only">checkbox</label>
+                    
                 
-            <Input placeholder= {veriIcerigi}  scope="row" class="py-4 bg-transparent font-medium text-xl px-6">
-                
-            </Input>
-          
+            </td>
         </tr>
     )
 }
